83c5f3463a0d68d0ea3f04d7d10d24b0336aa048
Mihir <mihir2726@gmail.com>
2022-03-14 12:29:48 +0000

    Index Complete excluding footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/assets");
 
   return {
     dir: {
