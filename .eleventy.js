781b58cd1a22962334f6a250271206f455df4be0
Mihir <mihir2726@gmail.com>
2022-03-24 15:45:20 +0000

    Fixed robots problem

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/robots.txt");
 
   eleventyConfig.addPlugin(timeToRead);
 
