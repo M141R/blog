70a7279a393fc84d795d3ca6322ce3b72cc85a00
Mihir <mihir2726@gmail.com>
2022-03-28 11:58:54 +0000

    Fixed template engine problem

--- .eleventy.js
+++ .eleventy.js
@@ -38,6 +38,10 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
+    markdownTemplateEngine: "md",
+
+    htmlTemplateEngine: "njk",
+
     dir: {
       input: "src",
       output: "public",
